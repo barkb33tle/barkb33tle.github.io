0c11b814b838bbdfe01bede837daa1d96823e5d4
Lukas VI <168646936+lukas-vis-mybanker@users.noreply.github.com>
2025-11-24 21:27:17 +0100

    Deploy

---
+++ _nuxt/CwbpIUG6.js
@@ -0,0 +1,1 @@
+import{_ as s,c as t,o,a5 as a}from"./IDOWRAUd.js";const c={},r={class:"page-shell"};function n(e,l){return o(),t("div",r,[a(e.$slots,"default")])}const d=s(c,[["render",n]]);export{d as default};
